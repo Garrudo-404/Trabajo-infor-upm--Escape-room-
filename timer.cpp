338f09a4771102dc001d31e978391ea9c5c2d7b9
Garrudo-404 <p.garrudo@alumnos.upm.es>
2022-04-26 20:35:35 +0200

    Create timer.cpp
    Timer para el juego. Al final suena un pitido.

---
+++ timer.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h> //system()
+#include <windows.h> //Sleep() & Beep()
+
+//funcion timer
+void timer(int h, int m, int s)
+{
+    while(1)
+    {
+        if (h == 0 && m == 0 && s == 0)
+        {
+            break;
+        }
+        if (s == 0 && m == 0)
+        {
+            m = 60;
+            h--;
+        }
+        if (s == 0)
+        {
+            s = 60;
+            m--;
+        }
+        system("cls");
+        printf("%d:%d:%d", h, m, s--);
+        Sleep(1000);
+    }
+}
+
+int main()
+{
+    int h, m, s;
+    h = 1;
+    m = 0;
+    s = 0;
+    timer(h, m, s);
+    for (int i = 100; ; i = i + 50)
+        Beep(i, 1000);
+}
