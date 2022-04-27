5043ebc26bc72f9bd528fe636c8171b683983f87
Garrudo-404 <p.garrudo@alumnos.upm.es>
2022-04-27 16:26:39 +0200

    Update timer.cpp

--- timer.cpp
+++ timer.cpp
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h> //system()
 #include <windows.h> //Sleep() & Beep()
-
+void timer(int, int, int);
+int main()
+{
+    int h, m, s;
+    h = 1;
+    m = 0;
+    s = 0;
+    timer(h, m, s);
+    for (int i = 100; ; i = i + 50)
+        Beep(i, 1000);
+}
 //funcion timer
 void timer(int h, int m, int s)
 {
-    while(1)
+    while (1)
     {
         if (h == 0 && m == 0 && s == 0)
         {
@@ -25,15 +35,4 @@ void timer(int h, int m, int s)
         printf("%d:%d:%d", h, m, s--);
         Sleep(1000);
     }
-}
-
-int main()
-{
-    int h, m, s;
-    h = 1;
-    m = 0;
-    s = 0;
-    timer(h, m, s);
-    for (int i = 100; ; i = i + 50)
-        Beep(i, 1000);
 }
